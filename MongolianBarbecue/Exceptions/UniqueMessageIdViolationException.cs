a5e3bbfbdabfe51bc1623c274a8eacada3c002ba
Mogens Heller Grabe <mookid8000@gmail.com>
2018-05-31 09:17:43 +0200

    fix compilation

--- MongolianBarbecue/Exceptions/UniqueMessageIdViolationException.cs
+++ MongolianBarbecue/Exceptions/UniqueMessageIdViolationException.cs
@@ -1,14 +1,36 @@
 ﻿using System;
+#if HAS_SERIALIZABLE_ATTRIBUTE
+using System.Runtime.Serialization;
+#endif
 
 namespace MongolianBarbecue.Exceptions
 {
+    /// <summary>
+    /// Exception thrown when a message with an explicitly set ID is sent, and a message already exists with that ID
+    /// </summary>
+#if HAS_SERIALIZABLE_ATTRIBUTE
+    [Serializable]
+#endif
     public class UniqueMessageIdViolationException : Exception
     {
+        /// <summary>
+        /// Gets the problematic ID
+        /// </summary>
         public string Id { get; }
 
+        /// <summary>
+        /// Constructs the exception with the given ID, generating a sensible message at the same time
+        /// </summary>
         public UniqueMessageIdViolationException(string id) : base($"Cannot send message with ID {id} because a message already exists with that ID")
         {
             Id = id;
         }
+
+#if HAS_APPDOMAINS
+        protected UniqueMessageIdViolationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+#endif
     }
 }
